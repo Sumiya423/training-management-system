746ea8368bab52c53ae8822a537a376ca4e20df2
Sumiya Alam Akhi <sumiya.alam@bjitgroup.com>
2022-07-13 10:26:41 +0600

    models-creation

--- Training Management Back/models/course.js
+++ Training Management Back/models/course.js
@@ -1,11 +1,6 @@
 const mongoose = require('mongoose')
 
-const batchSchema = new mongoose.Schema({
-
-    trainerId: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true
-    }
+const courseSchema = new mongoose.Schema({
     title:{
         type: String,
         required: true
